f44600c96a0b0dcf132d84d437ac88e3db4150a2
sergivb01 <sergivb01@gmail.com>
2018-06-28 17:19:28 +0200

    this project is shit af

--- src/main/java/site/solenxia/staffutilities/StaffUtilities.java
+++ src/main/java/site/solenxia/staffutilities/StaffUtilities.java
@@ -1,12 +1,9 @@
 package site.solenxia.staffutilities;
 
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import site.solenxia.staffutilities.commands.MuteChatCommand;
 import site.solenxia.staffutilities.commands.ReportCommand;
 import site.solenxia.staffutilities.commands.RequestCommand;
@@ -14,10 +11,7 @@ import site.solenxia.staffutilities.commands.StaffChatCommand;
 import site.solenxia.staffutilities.listeners.MuteChatListener;
 import site.solenxia.staffutilities.redis.RedisManager;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.Map;
 
 public class StaffUtilities extends JavaPlugin{
@@ -34,9 +28,6 @@ public class StaffUtilities extends JavaPlugin{
 			this.saveDefaultConfig();
 		}
 		this.getConfig().options().copyDefaults(true);
-//
-//		instance.Check();
-//		instance.Check2();
 
 		new RedisManager(this);
 
@@ -64,82 +55,4 @@ public class StaffUtilities extends JavaPlugin{
 	}
 
 
-	public void Check(){
-		try{
-			boolean license = false;
-			URL url = new URL("http://solenxia.site/antileak/admin/api.php?plugin=StaffUtilities=" + getConfig().getString("LICENSE_KEY"));
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-			String inputLine = in.readLine();
-			while(inputLine != null){
-				if(!(inputLine.contains("true")))
-					continue;
-				license = true;
-				PluginDescriptionFile pdf = this.getDescription(); //Gets plugin.yml
-				pdf.getVersion(); //Gets the version
-				System.out.println("-----------------------------------");
-				System.out.println(" ");
-				System.out.println("Solenxia StaffUtils - Version " + pdf.getVersion());
-				System.out.println(" ");
-				System.out.println("Successfuly verified license key.");
-				System.out.println(" ");
-				System.out.println("-----------------------------------");
-
-				return;
-			}
-			in.close();
-			if(!(license)){
-				System.out.println("Leaked version of Solenxia Hubcore detected, shutting down server.");
-				Bukkit.getPluginManager().disablePlugin(this);
-				this.leaked = true;
-				return;
-			}
-		}catch(Exception ex){
-			System.out.print("Connection issue with auth servers.");
-			System.out.print("Please report this to me on Discord.");
-			System.out.print(ex);
-		}
-
-		Bukkit.getPluginManager().disablePlugin(this);
-		this.leaked = true;
-		return;
-	}
-
-	public void Check2(){
-		try{
-			boolean license = false;
-			URL url = new URL("https://solenxia.site/antileak/admin/api.php?plugin=?plugin=StaffUtilities=" + getConfig().getString("LICENSE_KEY"));
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-			String inputLine = in.readLine();
-			while(inputLine != null){
-				if(!(inputLine.contains("true")))
-					continue;
-				license = true;
-
-				return;
-			}
-			in.close();
-			if(!(license)){
-				System.out.println("Leaked version of Solenxia Hubcore detected, shutting down server.");
-				Bukkit.getPluginManager().disablePlugin(this);
-				this.leaked = true;
-				return;
-			}
-		}catch(Exception ex){
-			System.out.print("Connection issue with auth servers.");
-			System.out.print("Please report this to me on Discord.");
-			System.out.print(ex);
-		}
-
-		Bukkit.getPluginManager().disablePlugin(this);
-		this.leaked = true;
-		return;
-	}
-
-	public void Timer(){
-		new BukkitRunnable(){
-			public void run(){
-				Check2();
-			}
-		}.runTaskTimerAsynchronously(this, 10L, 0L);
-	}
 }
